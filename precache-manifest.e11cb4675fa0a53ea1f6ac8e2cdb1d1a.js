f320eb8b50a9881694d307c9c9d89b1ce7a123b7
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2019-10-27 04:40:46 +0900

    Updates

---
+++ precache-manifest.e11cb4675fa0a53ea1f6ac8e2cdb1d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fbe35b9c7e2c20b93a0eeb25efa3b6",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "1f81d680faec20cde7fb",
+    "url": "/bingo/static/css/main.a84b07c4.chunk.css"
+  },
+  {
+    "revision": "bfe946767d2baa5af1fe",
+    "url": "/bingo/static/js/2.6d72cf9b.chunk.js"
+  },
+  {
+    "revision": "1f81d680faec20cde7fb",
+    "url": "/bingo/static/js/main.03bc26be.chunk.js"
+  },
+  {
+    "revision": "5e93ffebbd771a118273",
+    "url": "/bingo/static/js/runtime-main.a746cc04.js"
+  }
+]);
